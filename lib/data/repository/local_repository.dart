43e03f158b431def64df74029a742a65ba53da9e
qqOwOqq <skdnfl4343@gmail.com>
2023-05-14 16:46:43 +0900

    asset 경로 변경

--- lib/data/repository/local_repository.dart
+++ lib/data/repository/local_repository.dart
@@ -5,7 +5,7 @@ import '../model/schedule.dart';
 
 class ScheduleRepository{
   Future<List<Schedule>> getItemsFromJsonAssets() async {
-    String jsonString = await rootBundle.loadString('assets/data.json');
+    String jsonString = await rootBundle.loadString('asset/data.json');
     List<dynamic> jsonData = jsonDecode(jsonString);
     return jsonData.map((json) => Schedule.fromJson(json)).toList();
   }
