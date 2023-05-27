1e913220f339e0115188df8ddf34fa7b350493d2
qqOwOqq <skdnfl4343@gmail.com>
2023-05-27 10:23:05 +0900

    앱 컬러 추가(light grey, red)

--- lib/data/model/appColor.dart
+++ lib/data/model/appColor.dart
@@ -5,4 +5,6 @@ class AppColor{
   final Color mainColor = const Color(0xFFFF89B3);
   final Color mainColor88 = const Color(0x88FF89B3);
   final Color whiterColor = const Color(0xFFFFC0D6);
+  final Color lightGreyColor = const Color(0xFFEFEFEF);
+  final Color redColor = const Color(0xFFFF5656);
 }
