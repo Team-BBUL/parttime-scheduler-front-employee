b7a6a5ad9394f160e7ac72e70671f3dba67564b5
qqOwOqq <skdnfl4343@gmail.com>
2023-08-17 20:22:50 +0900

    DateTime 클래스 테스트

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,14 +4,29 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:sidam_employee/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+
+  int year = 2023;
+  int month = 12;
+
+  DateTime firstDayOfMonth = DateTime(year, month, 1);
+  DateTime lastDayOfMonth = DateTime(year, month + 1, 1);
+
+  print('첫 날: ${firstDayOfMonth.day}');
+  print('말일: ${lastDayOfMonth.day}');
+
+  print('년도 : ${firstDayOfMonth.year}');
+  print('한달 뒤 년도 : ${lastDayOfMonth.year}');
+
+
+  DateTime curMonth = DateTime(2023, 07, 31);
+  DateTime prevMonth = DateTime(2023, 07 - 1, 31+1);
+
+  print('현재 달 : ${curMonth}');
+  print('이전 달 : ${prevMonth}');
+
+
+/*  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
@@ -26,5 +41,5 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
-  });
+  });*/
 }
