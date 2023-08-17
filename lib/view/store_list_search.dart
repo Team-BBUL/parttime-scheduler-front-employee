76841c34299bc44323d50e734591d69e6f4a3387
qqOwOqq <skdnfl4343@gmail.com>
2023-08-17 20:35:31 +0900

    store 검색 스크린 구현

---
+++ lib/view/store_list_search.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sidam_employee/view/store_list_page.dart';
+import 'package:sidam_employee/view_model/store_view_model.dart';
+
+import '../main.dart';
+import '../util/appColor.dart';
+import '../util/dialog_message.dart';
+
+class StoreListSearchScreen extends StatelessWidget{
+
+  Widget build(BuildContext context) {
+    return Consumer<StoreViewModel>(
+        builder:(context, viewModel,child) {
+          return Scaffold(
+              appBar: AppBar(
+                title: Text('Search'),
+              ),
+              body: Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Flexible(
+                      flex : 1,
+                      child: Row(
+                        children: [
+                          Flexible(
+                            flex: 9,
+                            child: TextField(
+                              onChanged: (text) => viewModel.setSearchText(text),
+                              onSubmitted: (_) => viewModel.getStoresWithSearch(),
+                              decoration: const InputDecoration(
+                                hintText: '매장명을 입력해주세요',
+                                labelStyle: TextStyle(color: Colors.grey),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                  BorderSide(width: 1, color: Colors.grey),
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                  borderSide:
+                                  BorderSide(width: 1, color: Colors.grey),
+                                ),
+                                border: UnderlineInputBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                ),
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                          ),
+
+                          Flexible(flex: 1,
+                              child: GestureDetector(
+                                  onTap: () => viewModel.getStoresWithSearch(),
+                                  child: Icon(Icons.search))),
+                        ],
+                      ),
+                    ),
+                    Flexible(
+                        child: ListView.builder(
+                            itemCount:viewModel.stores?.length ?? 0,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.black,
+                                        width: 1,
+                                      )
+                                  ),
+                                  child: GestureDetector(
+                                      onTap:()=>{viewModel.setStore(index)},
+                                      child: Container(
+                                          child:Text("${viewModel.stores?[index].name ?? ''}")
+                                      )
+                                  )
+                              );
+                            }
+                        )
+                    ),
+                    viewModel.expanded ? Text(
+                      "매장명 : ${viewModel.storeInfo.name}\n 주소 :${viewModel.storeInfo.location}",
+                    ) : Container(),
+                    Flexible(
+                      child: Container(
+                        width: double.infinity,
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                          child: FilledButton(onPressed: () {
+                            Message().showConfirmDialog(
+                                context: context,
+                                title: "매장 등록",
+                                message: "매장을 등록하시겠습니까?",
+                              apiCall: () => viewModel.joinStore(),
+                            );
+                          },
+                            style: FilledButton.styleFrom(
+                              backgroundColor: AppColor().mainColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            child: Text("등록",
+                              style: TextStyle(fontSize: 17,color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+          );
+        }
+    );
+  }
+}
---
+++ lib/view/store_list_search_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:sidam_employee/view/store_list.dart';
+import 'package:sidam_employee/view/store_list_search.dart';
+import 'package:sidam_employee/view_model/store_view_model.dart';
+
+import '../data/repository/store_repository.dart';
+import '../data/repository/store_repository_mock.dart';
+
+class StoreListSearchPage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<StoreViewModel>(
+        create: (_) => StoreViewModel(StoreRepositoryImpl()),
+        child: StoreListSearchScreen());
+  }
+}
