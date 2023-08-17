53870ff79864659f1c7b75532581b8f30cb3daef
qqOwOqq <skdnfl4343@gmail.com>
2023-08-17 20:42:49 +0900

    근무 불가능 시간 스크린 관련 mvvm 모델 의존성 역전 수행 클래스 추가

---
+++ lib/view/unworkable_schedule_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sidam_employee/view/unworkable_schedule.dart';
+import 'package:sidam_employee/view_model/unworkable_schedule_view_model.dart';
+
+import '../data/repository/unworkable_schedule_repository.dart';
+
+class UnworkableSchedulePage extends StatelessWidget{
+  const UnworkableSchedulePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<UnworkableScheduleViewModel>(
+        create: (_) => UnworkableScheduleViewModel(ImpossibleScheduleRepository()),
+        child: const UnworkableScheduleScreen());
+  }
+}
