4e941180c87ceaac09e88b786503feeb81ed1f11
qqOwOqq <skdnfl4343@gmail.com>
2023-08-17 20:32:33 +0900

    로그인 검증 로직 추가

---
+++ lib/view/check_login.dart
@@ -0,0 +1,85 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sidam_employee/util/app_future_builder.dart';
+import 'package:sidam_employee/util/sp_helper.dart';
+import 'package:sidam_employee/view/login.dart';
+import 'package:sidam_employee/view/signup.dart';
+import 'package:sidam_employee/view/store_list_page.dart';
+
+import 'home.dart';
+class CheckLoginScreen extends StatefulWidget{
+
+  @override
+  _CheckLoginScreenState createState() => _CheckLoginScreenState();
+}
+
+class _CheckLoginScreenState extends State<CheckLoginScreen> {
+  SPHelper helper = SPHelper();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AppFutureBuilder<void>(
+        future: _checkLoginStatus(),
+        builder: (context, snapshot) {
+          return Container();
+        },
+      ),
+    );
+  }
+
+  _checkLoginStatus() async {
+    await helper.init();
+    await Future.delayed(Duration(seconds: 1));
+    bool isLoggedIn = helper.getIsLoggedIn();
+    int? currentStoreId = helper.getStoreId();
+    bool isRegistered = helper.getIsRegistered();
+    String jwt = helper.getJWT();
+    if(jwt.isEmpty) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+        ),
+      );
+    }
+    log(isLoggedIn ? '로그인됨' : '로그인안됨');
+    log(isRegistered ? '등록됨' : '등록안됨');
+    log(currentStoreId != null ? '$currentStoreId' : '가게선택안됨');
+    if(isLoggedIn && isRegistered && currentStoreId != null){
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomeScreen(),
+        ),
+      );
+    }else if (isLoggedIn && isRegistered) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => StoreListPage(),
+        ),
+      );
+    }else if(isLoggedIn){
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SignupScreen(),
+        ),
+      );
+    }else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginScreen(),
+        ),
+      );
+    }
+  }
+}
