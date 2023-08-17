f6f3ff960b53bd9c6fa8a150399487d91d60580f
qqOwOqq <skdnfl4343@gmail.com>
2023-08-17 20:38:05 +0900

    공지사항 스크린 추가

--- lib/view/announcement_detail.dart
+++ lib/view/announcement_detail.dart
@@ -1,107 +1,165 @@
 
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../util/app_future_builder.dart';
+import '../util/image_hero.dart';
 import '../view_model/announcement_view_model.dart';
+import 'enum/image_type.dart';
 
 
 class AnnouncementDetailScreen extends StatelessWidget {
-  const AnnouncementDetailScreen({super.key});
+  final int index;
+
+  AnnouncementDetailScreen(this.index);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(kToolbarHeight), // AppBar의 높이 지정
-        child: Consumer<AnnouncementViewModel>(
-          builder: (context, viewModel, _) {
-            return AppBar(
-              title: Text('view mode'),
-            );
-          },
-        ),
-      ),
-      body: Consumer<AnnouncementViewModel>(
-          builder:(context, viewModel,child){
-            return SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                            child: Container(
-                                margin: const EdgeInsets.symmetric(vertical: 5,horizontal: 10),
-                                child:Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: Column(
-                                    children: [
-                                        Text(
-                                          '${viewModel.announcement.subject}${viewModel.announcement.id}' ?? '',
-                                          style: TextStyle(fontSize: 16),
-                                        ),
-                                    ],
-                                  ),
-                                )
-                            )
-                        ),
-                      ],
+    final viewModel = Provider.of<AnnouncementViewModel>(context,listen: true );
+    return AppFutureBuilder<void>(
+      future: viewModel.getAnnouncement(index),
+      builder: (context, snapshot){
+        return Scaffold(
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(kToolbarHeight), // AppBar의 높이 지정
+              child: AppBar(
+                title: const Text('View Mode'),
+              ),
+            ),
+            body: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Flexible(
+                    flex: 1,
+                    child: Column(
+                        children:[
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 10),
+                            alignment: Alignment.centerLeft,
+                            padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                            decoration:  const BoxDecoration(),
+                            child: Text(
+                              viewModel.announcement?.subject ?? '',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.centerLeft,
+                            padding: EdgeInsets.fromLTRB(30, 5, 0, 5),
+                            child: Text(
+                                viewModel.announcement?.timeStamp != null
+                                    ? '${viewModel.announcement?.timeStamp?.year}년 '
+                                    '${viewModel.announcement?.timeStamp?.month}월 '
+                                    '${viewModel.announcement?.timeStamp?.day}일 '
+                                    '${viewModel.announcement?.koreanWeekday()}요일': '',
+                                style: const TextStyle(fontSize: 14, color: Colors.grey)),
+                          ),
+                        ]
+                    )
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 0,horizontal: 10),
+                  decoration: const BoxDecoration(
+                    border: Border(
+                        bottom : BorderSide(width: 1, color: Colors.grey)
                     ),
-                    Row(
-                      children: [
-                        Expanded(
-                            child: Container(
-                                margin: const EdgeInsets.symmetric(vertical: 5,horizontal: 10),
-                                alignment: Alignment.centerLeft,
-                                child: Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: Column(
-                                    children: [
-                                        Text(
-                                          viewModel.announcement.body ?? '',
-                                          style: TextStyle(fontSize: 16),
-                                        ),
-                                    ],
-                                  ),
-                                )
-                            )
+                  ),
+                ),
+                Flexible(
+                    flex: 7,
+                    child: Container(
+                        margin: const EdgeInsets.symmetric(vertical: 5,horizontal: 10),
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text(
+                              viewModel.announcement?.content ?? '',
+                              style: TextStyle(fontSize: 16)),
+
                         )
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            physics: NeverScrollableScrollPhysics(),
-                            itemCount: viewModel.imagePaths.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                leading: Image.file(
-                                  File(viewModel.imagePaths[index]),
-                                  fit: BoxFit.cover,
-                                ),
-                                title: Text(viewModel.imagePaths[index]),
-                              );
-                            },
+                    )
+                ),
+                Flexible(
+                    flex: 3,
+                    child: Container(
+                        alignment: Alignment.centerLeft,
+                        padding: EdgeInsets.fromLTRB(10, 5, 0, 5),
+                        margin: const EdgeInsets.symmetric(vertical: 0,horizontal: 10),
+                        decoration: BoxDecoration(
+                          border: const Border(
+                              top : BorderSide(width: 1, color: Colors.grey)
                           ),
                         ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(8),
-                          child: Text(viewModel.announcement.timestamp ?? '', style: TextStyle(fontSize: 16)),
-                        ),
-                      ],
+                        child: Row(
+                          children: [
+                            ListView.builder(
+                              shrinkWrap: true,
+                              scrollDirection: Axis.horizontal,
+                              itemCount: viewModel.images?.length ?? 0,
+                              itemBuilder: (context, index) {
+                                if(viewModel.images![index] is File){
+                                  return GestureDetector(
+                                    onTap: (){
+                                      Navigator.push(context, MaterialPageRoute(
+                                          builder: (context) {
+                                            return ImageHero(image : viewModel.images![index].path, type: ImageType.FILE);
+                                          }
+                                      ));
+                                    },
+                                    child: Container(
+                                      width: 100,
+                                      height: 100,
+                                      margin: const EdgeInsets.symmetric(vertical: 0,horizontal: 2),
+                                      alignment: Alignment.centerLeft,
+                                      child: Padding(
+                                        padding: EdgeInsets.all(8),
+                                        child: Image.file(File(viewModel.images![index].path)
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                }else{
+                                  return GestureDetector(
+                                    onTap: (){
+                                      Navigator.push(context, MaterialPageRoute(
+                                          builder: (context) {
+                                            return ImageHero(image : viewModel.images![index], type: ImageType.MEMORY);
+                                          }
+                                      ));
+                                    },
+                                    child: Container(
+                                      width: 100,
+                                      height: 100,
+                                      margin: const EdgeInsets.symmetric(vertical: 0,horizontal: 2),
+                                      alignment: Alignment.centerLeft,
+                                      child: Padding(
+                                        padding: EdgeInsets.all(8),
+                                        child: Image.memory(
+                                          viewModel.images![index],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                }
+
+                              },
+                            ),
+                            // Container(),
+                          ],
+                        )
                     )
-                  ],
-                )
-            );
-          }
-      ),
+
+                ),
+
+              ],
+            )
+
+        );
+      },
     );
+
   }
 }
